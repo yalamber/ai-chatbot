32ddfba591a6b0b4d95b78d46e9f1b72f2769091
Yalamber Subba <yalamber@mundhum.com>
2024-07-29 21:04:05 +0545

    updates

--- components/collection.tsx
+++ components/collection.tsx
@@ -21,9 +21,7 @@ export async function Collection({ userId }: CollectionPageProps) {
     <div className={'p-5'}>
       <div className="lg:flex lg:items-center lg:justify-between">
         <div className="min-w-0 flex-1">
-          <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
-            Collection
-          </h2>
+          <h1 className="text-xl font-bold sm:text-2xl">Collection</h1>
         </div>
         <div className="mt-5 flex lg:ml-4 lg:mt-0">
           <CollectionCreateButton />
