ece1cc7e9de957a4dbab3175433b46d8dbcf36a4
Yalamber <yalamber@mundhum.com>
2024-07-31 07:38:00 +0545

    Fix button color

--- components/sidebar-desktop.tsx
+++ components/sidebar-desktop.tsx
@@ -21,8 +21,8 @@ export async function SidebarDesktop() {
             <Link
               href="/new-thread"
               className={cn(
-                buttonVariants({ variant: 'default' }),
-                'h-10 w-full '
+                buttonVariants({ variant: 'outline' }),
+                'h-10 w-full justify-start bg-zinc-50 px-4 shadow-none transition-colors hover:bg-zinc-200/40 dark:bg-zinc-900 dark:hover:bg-zinc-300/10'
               )}
             >
               Threads +
